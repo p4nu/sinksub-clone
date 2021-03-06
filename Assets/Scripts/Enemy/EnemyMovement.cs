8984b3d81021440a12e2b9e2bffc18b99a71c353
Panu Valtanen <p4nu@users.noreply.github.com>
2021-03-06 17:51:42 +0200

    Add dumb enemy.

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+        [SerializeField] private float speed = 5f;
+
+        private void FixedUpdate()
+        {
+            transform.Translate(speed * Time.fixedDeltaTime * Vector2.right);
+        }
+    }
+}
