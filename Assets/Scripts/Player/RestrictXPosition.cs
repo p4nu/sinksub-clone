4ed1483bb21093cdd1e532881ea53d8b6f344793
Panu Valtanen <p4nu@users.noreply.github.com>
2021-03-07 12:51:04 +0200

    Move RestrictXPosition.cs to Player folder.

---
+++ Assets/Scripts/Player/RestrictXPosition.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(PlayerMovement))]
+    public class RestrictXPosition : MonoBehaviour
+    {
+        [SerializeField] private float maxX = 8f;
+
+        private PlayerMovement _playerMovement;
+
+        private void Awake()
+        {
+            _playerMovement = GetComponent<PlayerMovement>();
+        }
+
+        private void Update()
+        {
+            ClampPosition();
+
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (Mathf.Abs(transform.position.x) == maxX)
+            {
+                _playerMovement.ResetVelocity();
+            }
+        }
+
+        private void ClampPosition()
+        {
+            var transformPosition = transform.position;
+
+            transformPosition.x = Mathf.Clamp(transformPosition.x, -maxX, maxX);
+
+            transform.position = transformPosition;
+        }
+    }
+}
