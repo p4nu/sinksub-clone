a804164ed474c976ca3d1d9f67c018dc6187030f
Panu Valtanen <p4nu@users.noreply.github.com>
2021-03-07 12:59:44 +0200

    Define max and min speed values for the Enemy. Add some random speed to enemy in Start event.

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Enemy
 {
     public class EnemyMovement : MonoBehaviour
     {
-        [SerializeField] private float speed = 5f;
+        [SerializeField] private float minSpeed = 0.2f;
+        [SerializeField] private float maxSpeed = 1f;
+
+        private float _speed;
+
+        private void Start()
+        {
+            _speed = Random.Range(minSpeed, maxSpeed);
+        }
 
         private void FixedUpdate()
         {
-            transform.Translate(speed * Time.fixedDeltaTime * Vector2.right);
+            transform.Translate(_speed * Time.fixedDeltaTime * Vector2.right);
         }
     }
 }
